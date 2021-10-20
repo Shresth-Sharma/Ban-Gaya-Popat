39f0247133b3665f7d4bf8f5751b491b38a78a31
Shresth-Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2021-10-20 13:31:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+function setup(){
+ createCanvas(windowWidth,windowHeight);
+ textSize(40);
+ text("Your device is in a very old model",windowWidth/2-windowWidth/3,windowHeight/3);
+ text("Please upgrage your device",windowWidth/2-windowWidth/3,windowHeight/2);
+}
